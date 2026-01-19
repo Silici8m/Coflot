641f8adaa44ed5d7a42460929c1057d8020a4784
Victor <vik.forc38@gmail.com>
2026-01-19 22:57:47 +0100

    update des topic et msg

--- IHM/v12/js/Config.js
+++ IHM/v12/js/Config.js
@@ -2,9 +2,12 @@ const CONFIG = {
     ROSBRIDGE_URL: 'ws://localhost:9090',
     TYPE_POSE:     'geometry_msgs/msg/PoseStamped',
 
-    TOPIC_MISSION: '/fleet/mission_request',
-    TYPE_MISSION: 'fleet_simulator/msg/MissionRequest',
+    TOPIC_MISSION_REQUEST: '/mission/mission_request',
+    TYPE_MISSION_REQUEST: 'fleet_interfaces/msg/MissionRequest',
 
-    TOPIC_FLEET:   '/fleet/fleet_state',
-    TYPE_FLEET:   'fleet_simulator/msg/FleetState' 
+    TOPIC_FLEET_STATE:   '/fleet/fleet_state',
+    TYPE_FLEET_STATE:   'fleet_interfaces/msg/RobotStateArray' ,
+
+    TOPIC_MISSIONS:   '/mission/missions_state',
+    TYPE_MISSIONS:   'fleet_interfaces/msg/RobotStateArray' 
 };
